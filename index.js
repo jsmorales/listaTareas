1bcee938641b5639284d277c924570505bc740df
johanmorales <jhellmetal2000@gmail.com>
2017-03-30 17:59:33 -0500

    Se muestra como enbeber codigo en la vista html para poder mostrar data segun parametros pasados por el metodo render, tambien se estudia como se manejan las peticiones y sus parametros dentro del llamado en el server.

--- index.js
+++ index.js
@@ -34,7 +34,32 @@ app.set('view engine', 'ejs')
 //para servir las paginas 
 //home
 app.get('/', function(llamado, respuesta){
-	respuesta.render('formulario.ejs', {})
+	//render envia un objeto con datos que pueden ser
+	//mostrados en dicha pagina parecido a RoR
+	respuesta.render('formulario.ejs', {'tareas': tareas})
+	console.log(tareas)
+})
+//-----------------------------------------------------------
+
+//-----------------------------------------------------------
+var tareas = []; //array que va a contener las tareas
+
+//para recibir los datos por post se configura
+app.post('/adicionar', function(llamado, respuesta){
+	//guarda el dato de la tarea
+	var tarea = llamado.body.nuevaTarea;
+	//guarda la tarea en el array
+	tareas.push(tarea)
+	//redirecciona al formulario.ejs
+	respuesta.redirect('/')
+})
+
+//para eliminar los datos se configura la peticion
+app.get("/borrar/:id", function(llamado, respuesta){
+	//retira la tarea del array con el metodo splice
+	//javascript
+	tareas.splice(llamado.params.id, 1)
+	respuesta.redirect('/')
 })
 //-----------------------------------------------------------
 
