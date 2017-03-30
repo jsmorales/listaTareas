7e5119a6dce7c36411e279c539da1c1aa3f1714e
johanmorales <jhellmetal2000@gmail.com>
2017-03-30 17:02:54 -0500

    Se estudia como crear una vista con bootstrap pero con cdns y como servirla con nuestra app

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 //Acá es donde se escribe nuestra aplicación
+//Se hace la inclusion de los modulos necesarios
+//-----------------------------------------------------------
+var express = require('express')
+var bodyParser = require('body-parser')
+var session = require('cookie-session')
+//-----------------------------------------------------------
+
+//-----------------------------------------------------------
+//se inicializa la aplicacion en una variable llamada app
+//la cual es el server como tal
+var app = express();
+//-----------------------------------------------------------
+
+//-----------------------------------------------------------
+//para poder guardar la info en nuestra app se usa session
+//la cual se escribe asi:
+app.use(session({secret:'nodejs'}))
+//secret es por seguridad
+//-----------------------------------------------------------
+
+//-----------------------------------------------------------
+//para poder recibir datos en evento POST se usa el bodyParser
+//y no es de tipo extendido, esto es solo para forms anidados
+app.use(bodyParser.urlencoded({extended:false}))
+//-----------------------------------------------------------
+
+//-----------------------------------------------------------
+//para las vistas se usa ejs, el cual se setea asi
+app.set('view engine', 'ejs')
+//-----------------------------------------------------------
+
+//-----------------------------------------------------------
+//para servir las paginas 
+//home
+app.get('/', function(llamado, respuesta){
+	respuesta.render('formulario.ejs', {})
+})
+//-----------------------------------------------------------
+
+//-----------------------------------------------------------
+//luego se empieza a correr la app con listen
+app.listen(3000, function(){
+	console.log("Server en localhost:3000")
+})
+//-----------------------------------------------------------
